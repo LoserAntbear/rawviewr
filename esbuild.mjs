c9f4a8ac36f2c1760f16f3dde97c6078740967c5
Roman Dyro <tollemet@gmail.com>
2026-07-28 19:26:36 +0200

    Update esbuild config with problem matcher, fixd async eror rpoerting and typings

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,39 +1,83 @@
 import * as esbuild from 'esbuild';
 
+// region: esbuild problem matcher plugin
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+  name: 'esbuild-problem-matcher',
+
+  setup(build) {
+    build.onStart(() => {
+      console.log('[watch] build started');
+    });
+    build.onEnd(result => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        if (location == null) return;
+        console.error(`    ${location.file}:${location.line}:${location.column}:`);
+      });
+      console.log('[watch] build finished');
+    });
+  }
+};
+// endregion
+
 const production = process.argv.includes('--production');
 const watch = process.argv.includes('--watch');
 
+// region: main
 /** @type {import('esbuild').BuildOptions} */
-const common = {
+const sharedConfig = {
   bundle: true,
+  logLevel: 'info',
   minify: production,
   sourcemap: !production,
-  logLevel: 'info',
+  plugins: [
+    /* esbuildProblemMatcherPlugin should go to the end of plugins array */
+    esbuildProblemMatcherPlugin
+  ]
 };
 
-const builds = [
+/** @type {import('esbuild').BuildOptions[]} */
+const buildConfigs = [
   {
-    ...common,
-    entryPoints: ['src/extension.ts'],
-    outfile: 'dist/extension.js',
-    platform: 'node',
+    ...sharedConfig,
     format: 'cjs',
+    platform: 'node',
     target: 'node18',
     external: ['vscode'],
+    outfile: 'dist/extension.js',
+    entryPoints: ['src/extension.ts'],
   },
   {
-    ...common,
-    entryPoints: ['src/webview/main.ts'],
-    outfile: 'dist/webview/main.js',
-    platform: 'browser',
+    ...sharedConfig,
     format: 'iife',
     target: 'es2020',
+    platform: 'browser',
+    outfile: 'dist/webview/main.js',
+    entryPoints: ['src/webview/main.ts'],
   },
 ];
 
-if (watch) {
-  const contexts = await Promise.all(builds.map((b) => esbuild.context(b)));
-  await Promise.all(contexts.map((c) => c.watch()));
-} else {
-  await Promise.all(builds.map((b) => esbuild.build(b)));
+/**
+ *
+ * @param {import('esbuild').BuildOptions[]} configs
+ */
+async function main(configs) {
+  try {
+    if (watch) {
+      const contexts = await Promise.all(configs.map((b) => esbuild.context(b)));
+
+      await Promise.all(contexts.map((c) => c.watch()));
+    } else {
+      await Promise.all(configs.map((b) => esbuild.build(b)));
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
+
+main(buildConfigs);
+// endregion
