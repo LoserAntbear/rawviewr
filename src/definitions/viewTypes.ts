60a8fe8bfd95c16c62dc7c11770d1586b7b48c5d
Roman Dyro <tollemet@gmail.com>
2026-07-31 00:17:24 +0200

    Add `ViewType` enum

---
+++ src/definitions/viewTypes.ts
@@ -0,0 +1,6 @@
+// Must match the ones defined in package.json
+export enum ViewType {
+  Single = 'rawImageViewer.editor',
+  Optional = 'rawImageViewer.editor.optional',
+  Gallery = 'rawImageViewer.gallery',
+}
