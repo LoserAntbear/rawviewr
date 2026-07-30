d4cf37dc3f1d57613d548cff70e9713417e98cab
Roman Dyro <tollemet@gmail.com>
2026-07-31 00:17:04 +0200

    Add `COMMAND_NAMES` enum

---
+++ src/definitions/commands.ts
@@ -0,0 +1,7 @@
+export enum COMMAND_NAMES {
+  open = 'rawImageViewer.open',
+  exportFile = 'rawImageViewer.exportFile',
+  openGallery = 'rawImageViewer.openGallery',
+  resetSettings = 'rawImageViewer.resetSettings',
+  openFolderGallery = 'rawImageViewer.openFolderGallery',
+}
