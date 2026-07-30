32d845fd5c10ca2e221c4400528dc80afd862813
Roman Dyro <tollemet@gmail.com>
2026-07-31 00:19:06 +0200

    Add `ExtensionHost` impl

---
+++ src/extension/ExtensionHost.ts
@@ -0,0 +1,57 @@
+import * as vscode from 'vscode';
+import { RawEditorProvider } from '../features/editor/RawEditorProvider';
+import { ViewType } from '../definitions/viewTypes';
+import { CommandDescriptor } from './types';
+import * as COMMAND_HANDLERS from './handlers';
+import { CommandNames } from '../definitions/commands';
+
+const EDITOR_OPTIONS = {
+  webviewOptions: { retainContextWhenHidden: true },
+  supportsMultipleEditorsPerDocument: true,
+};
+
+export class ExtensionHost {
+  constructor(
+    private readonly context: vscode.ExtensionContext,
+    private readonly provider: RawEditorProvider,
+  ) {}
+
+  public register(): void {
+    this.context.subscriptions.push(
+      ...this.registerCustomEditorProviders(),
+      ...this.registerCommands([
+        {
+          name: CommandNames.open,
+          action: COMMAND_HANDLERS.handleOpen,
+        },
+        {
+          name: CommandNames.openGallery,
+          action: COMMAND_HANDLERS.handleOpenGallery,
+        },
+        {
+          name: CommandNames.openFolderGallery,
+          action: COMMAND_HANDLERS.handleOpenFolderGallery,
+        },
+        {
+          name: CommandNames.exportFile,
+          action: COMMAND_HANDLERS.handleExport,
+        },
+        {
+          name: CommandNames.resetSettings,
+          action: () => COMMAND_HANDLERS.handleResetSettings(this.context.workspaceState),
+        },
+      ]),
+    );
+  }
+
+  private registerCustomEditorProviders(): vscode.Disposable[] {
+    return [
+      vscode.window.registerCustomEditorProvider(ViewType.Single, this.provider, EDITOR_OPTIONS),
+      vscode.window.registerCustomEditorProvider(ViewType.Optional, this.provider, EDITOR_OPTIONS),
+    ];
+  }
+
+  private registerCommands(commands: CommandDescriptor[]): vscode.Disposable[] {
+    return commands.map(({ name, action }) => vscode.commands.registerCommand(name, action));
+  }
+}
