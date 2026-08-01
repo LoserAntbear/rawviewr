4cc6416ce9dbe77a73a5648d5a6fa99cfaac9fcb
Roman Dyro <tollemet@gmail.com>
2026-08-01 16:35:40 +0200

    Update `RawEditorProvider` with DI registry implementation

--- src/features/editor/RawEditorProvider.ts
+++ src/features/editor/RawEditorProvider.ts
@@ -1,26 +1,30 @@
 import * as vscode from 'vscode';
-import { RawDocument } from '../document/RawDocument.js';
-import { Viewer, sourceFor } from '../../viewer.js';
-import { register } from '../../viewerRegistry.js';
+import { RawDocument } from '../document/RawDocument';
+import { Viewer } from '../../viewer';
+import type { ViewerRegistry } from '../viewer/registry/viewerRegistry';
+import { viewerSourceFor } from '../vscode/uri';
 
 export class RawEditorProvider implements vscode.CustomReadonlyEditorProvider<RawDocument> {
-  constructor(private readonly context: vscode.ExtensionContext) {}
+  constructor(
+    private readonly context: vscode.ExtensionContext,
+    private readonly viewerRegistry: ViewerRegistry,
+  ) {}
 
   public openCustomDocument(uri: vscode.Uri): RawDocument {
     return new RawDocument(uri);
   }
 
   public resolveCustomEditor(document: RawDocument, panel: vscode.WebviewPanel): void {
-    register(
-      panel,
-      new Viewer(
-        this.context,
-        panel.webview,
-        'single',
-        document.uri.toString(),
-        [
-          sourceFor(document.uri),
-        ]),
+    const viewer = new Viewer(
+      this.context,
+      panel.webview,
+      'single',
+      document.uri.toString(),
+      [
+        viewerSourceFor(document.uri),
+      ],
     );
+
+    this.viewerRegistry.register({ panel, viewer });
   }
 }
