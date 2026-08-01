72e21eef41631c7fa8195a538222bc217cfe542d
Roman Dyro <tollemet@gmail.com>
2026-08-01 16:31:40 +0200

    Add `ViewerRegistry` definition

---
+++ src/features/viewer/registry/viewerRegistry.ts
@@ -0,0 +1,28 @@
+import type { Viewer } from '../../../viewer.js';
+import { ViewerRegistryEntry } from './types.js';
+
+export class ViewerRegistry {
+  public get activeViewer(): Viewer | undefined {
+    for (const entry of this._registry) {
+      if (entry.panel.active) {
+        return entry.viewer;
+      }
+    }
+
+    return undefined;
+  }
+
+  private readonly _registry: Set<ViewerRegistryEntry> = new Set();
+
+  public register(entry: ViewerRegistryEntry): void {
+    this._registry.add(entry);
+    this.subscribeToPanelDisposal(entry);
+  }
+
+  private subscribeToPanelDisposal(entry: ViewerRegistryEntry): void {
+    entry.panel.onDidDispose(() => {
+      this._registry.delete(entry);
+      entry.viewer.dispose();
+    });
+  }
+}
