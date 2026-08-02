6b71270fafaf8bef713ec8edac79327b94e37849
Roman Dyro <tollemet@gmail.com>
2026-08-02 15:44:13 +0200

    Add `ALLOWED_DOCUMENT_SCHEMES` vscode def

--- src/definitions/vscode.ts
+++ src/definitions/vscode.ts
@@ -1,3 +1,6 @@
 export enum VSCodeCommands {
   OpenWith = 'vscode.openWith',
 }
+
+/** Document Schemes we are willing to read buffers from. */
+export const ALLOWED_DOCUMENT_SCHEMES: ReadonlySet<string> = new Set(['file', 'vscode-remote', 'vscode-vfs']);
