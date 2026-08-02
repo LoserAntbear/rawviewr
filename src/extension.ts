9485db6e4a6ecab1be3eac38565c705ad4b6f37a
Roman Dyro <tollemet@gmail.com>
2026-08-03 00:03:10 +0200

    Update extension declarations

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,21 @@ import * as vscode from 'vscode';
 
 import { RawEditorProvider } from './features/editor/RawEditorProvider';
 import { ExtensionHost } from './features/extension/ExtensionHost';
-import { ViewerWindowController } from './features/viewer/windowController/ViewerWindowController';
+import { IntentDispatcher } from './features/intent/IntentDispatcher';
+import { settingsResolvers } from './features/settings/resolvers';
 import { ViewerRegistry } from './features/viewer/registry/viewerRegistry';
-
+import { viewerIntentResolvers } from './features/viewer/resolvers';
+import { ViewerWindowController } from './features/viewer/windowController/ViewerWindowController';
 
 export function activate(context: vscode.ExtensionContext): void {
   const registry = new ViewerRegistry();
-  const provider = new RawEditorProvider(
-    context,
-    registry,
-  );
-  const host = new ExtensionHost(
-    context,
-    provider,
-    new ViewerWindowController(context, registry),
-  );
+  const windowController = new ViewerWindowController(context, registry);
+
+  const dispatcher = new IntentDispatcher({
+    ...viewerIntentResolvers(windowController, registry),
+    ...settingsResolvers(context.workspaceState),
+  });
+  const host = new ExtensionHost(context, new RawEditorProvider(context, registry), dispatcher);
 
   host.register();
 }
