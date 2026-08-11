c0833c8272e56ea9968237a30f1258fcf554e5bb
Roman Dyro <tollemet@gmail.com>
2026-08-11 18:15:46 +0200

    Setup eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+  files: ['**/*.{js,ts}'],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+  ],
+  rules: {
+    "no-restricted-syntax": [
+      "error",
+      // The rule is not so robust, but it will catch most cases of `disposables` being used in a class that does not extend `DisposableStore`.
+      // Maybe I'll add some custom AST traversal logic in the future to make it more robust, but for now this should be good enough.
+      {
+        "selector": ":matches(ClassDeclaration, ClassExpression):not([superClass.name='DisposableStore']) ClassBody > PropertyDefinition[key.name='disposables']",
+        "message": "Restricted use of `disposables`. Please extend `DisposableStore` instead. This also provides self-registration and safe cleanup on extension offload."
+      }
+    ]
+  }
+});
