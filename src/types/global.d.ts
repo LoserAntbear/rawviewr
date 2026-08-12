8437ae19fa56154d696c91f43a1d6f07dcf844ea
Roman Dyro <tollemet@gmail.com>
2026-08-12 17:20:12 +0200

    Add html and css files to TS import resolution

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,1 +1,12 @@
+declare module '*.html' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
+
+
 declare type Strip<T extends string, P extends string> = T extends `${P}.${infer R}` ? R : never;
