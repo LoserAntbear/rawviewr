759da13013dac1880747912bd0a7ef8a4b44892b
Roman Dyro <tollemet@gmail.com>
2026-08-12 17:37:20 +0200

    Add `acquireVsCodeApi` global typing definition for resolution

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -10,3 +10,15 @@ declare module '*.css' {
 
 
 declare type Strip<T extends string, P extends string> = T extends `${P}.${infer R}` ? R : never;
+
+/**
+ * VSCode-provided API for webviews, see https://code.visualstudio.com/api/extension-guides/webview#passing-messages-from-a-webview-to-an-extension
+ */
+declare function acquireVsCodeApi<
+  State = unknown,
+  Message = unknown,
+>(): {
+  getState: () => State;
+  setState: (newState: State) => void;
+  postMessage: (message: Message) => void;
+};
