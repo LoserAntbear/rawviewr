e839a818c20b5d5231366b394ec9566d76ed0cd4
Roman Dyro <tollemet@gmail.com>
2026-08-13 18:23:43 +0200

    Add early returns for `registerCustomComponents`

--- src/features/webview/main.ts
+++ src/features/webview/main.ts
@@ -6,6 +6,16 @@ const CUSTOM_COMPONENTS = [
 
 function registerCustomComponents(): void {
   for (const component of CUSTOM_COMPONENTS) {
+    if (!component.tagName) {
+      console.warn(`Cannot register custom component ${component.name} because it does not have a tagName property. Skipping registration.`);
+      continue;
+    }
+
+    if (customElements.get(component.tagName)) {
+      console.warn(`Custom component ${component.tagName} is already registered. Skipping registration.`);
+      continue;
+    }
+
     customElements.define(component.tagName, component);
   }
 }
