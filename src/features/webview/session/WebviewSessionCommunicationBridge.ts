150f932018416b594a9ef1c760bf4faa0f9c7c50
Roman Dyro <tollemet@gmail.com>
2026-08-14 00:19:41 +0200

    Rename `WebviewCommunicationBridge` to `WebviewSessionCommunicationBridge` for a better verbosity and move it to the own namespace

--- src/features/webview/session/WebviewSessionCommunicationBridge.ts
+++ src/features/webview/session/WebviewSessionCommunicationBridge.ts
@@ -1,9 +1,9 @@
-import { HostMessage, WebviewMessage } from './types';
+import { HostMessage, WebviewMessage } from '../types';
 
 // Can be called ONLY ONCE, thus safer to make as a global singleton
 const VSCODE_API = acquireVsCodeApi<unknown, WebviewMessage>();
 
-export type WebviewCommunicationBridge = {
+export type WebviewSessionCommunicationBridge = {
   postToWebviewHost: (message: WebviewMessage) => void;
   handleFromWebviewHost: (handler: (message: HostMessage) => void) => void;
 };
@@ -11,12 +11,14 @@ export type WebviewCommunicationBridge = {
 /**
  * There's virtually no reason to have more than one instance of this bridge, so let's make it a singleton.
  */
-export const WEBVIEW_COMM_BRIDGE: WebviewCommunicationBridge = {
+export const WEBVIEW_SESSION_COMM_BRIDGE: WebviewSessionCommunicationBridge = {
   postToWebviewHost: (message: WebviewMessage) => {
+    console.log('WebviewSessionCommunicationBridge: Posting message to webview host:', message);
     VSCODE_API.postMessage(message);
   },
   handleFromWebviewHost: (handler: (message: HostMessage) => void) => {
     window.addEventListener('message', (event: MessageEvent<HostMessage>) => {
+      console.log('WebviewSessionCommunicationBridge: Received message from webview host:', event.data);
       handler(event.data);
     });
   },
