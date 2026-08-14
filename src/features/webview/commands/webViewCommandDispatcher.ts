6a98f72c44fa383abaa9e545f9a5b39ef65a67fc
Roman Dyro <tollemet@gmail.com>
2026-08-14 17:51:56 +0200

    Add target listener subscription to `WebViewCommandDispatcher`

--- src/features/webview/commands/webViewCommandDispatcher.ts
+++ src/features/webview/commands/webViewCommandDispatcher.ts
@@ -1,16 +1,25 @@
-import type { WebviewCommand, WebviewCommandResolversMap } from './types';
+import type { WebviewCommand, WebviewCommandResolversMap, WebviewCommandResolver } from './types';
+import { RIV_COMMAND_EVENT_ID } from './definitions';
+import { listenTo } from '../disposable/listenTo';
+import type { WebviewDisposable } from '../disposable/types';
 
 export class WebViewCommandDispatcher {
-  constructor(private readonly resolvers: WebviewCommandResolversMap) {}
+  constructor(
+    private readonly resolvers: WebviewCommandResolversMap,
+  ) {}
 
-  public dispatch(command: WebviewCommand, payload: unknown): void {
-    const resolver = this.resolvers[command.kind];
+  public dispatch(command: WebviewCommand): void {
+    const resolver = this.resolvers[command.kind] as WebviewCommandResolver;
 
-    if (!resolver) {
-      console.warn(`No resolver found for command: ${command.kind}`);
-      return;
-    }
+    resolver(command);
+  }
 
-    resolver(command, payload);
+  /**
+   * Do not forget to dispose to detach listener
+   */
+  public listen(target: EventTarget): WebviewDisposable {
+    return listenTo(target, RIV_COMMAND_EVENT_ID, (event: Event) => {
+      this.dispatch((event as CustomEvent<WebviewCommand>).detail);
+    });
   }
 }
