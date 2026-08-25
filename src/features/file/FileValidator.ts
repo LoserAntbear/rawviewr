7c19d524e87a8d964f262ef006413e58024b49bb
Roman Dyro <tollemet@gmail.com>
2026-08-25 13:55:33 +0200

    Update fileValidator with file size reader

--- src/features/file/FileValidator.ts
+++ src/features/file/FileValidator.ts
@@ -2,9 +2,13 @@ import { AppContextProvider } from '@features/appContext/AppContextProvider';
 import { ByteConverter } from '@utils/bits';
 
 export class FileValidator {
-  public static validateFileSize(
+  public static get maxFileSizeMB(): number {
+    return AppContextProvider.context.workspaceConfig.read('maxFileSizeMB');
+  }
+
+  public static isValidFileSize(
     fileSize: number,
-    maxFileSizeMb: number = AppContextProvider.context.workspaceConfig.read('maxFileSizeMB'),
+    maxFileSizeMb: number = FileValidator.maxFileSizeMB,
   ): boolean {
     const maxFileSize = Math.max(1, ByteConverter.from('MB', maxFileSizeMb));
 
