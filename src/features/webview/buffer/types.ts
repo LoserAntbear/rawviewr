fd91d4ac4de8aa07715fa779868612f1c100eded
Roman Dyro <tollemet@gmail.com>
2026-08-27 00:19:00 +0200

    Add buffer `data` to `BufferBuildPayload`

--- src/features/webview/buffer/types.ts
+++ src/features/webview/buffer/types.ts
@@ -1,8 +1,9 @@
 export type BufferBuildPayload = {
   id: string;
   name: string;
+  data: ArrayBuffer;
 
   detail?: string;
+  // If you ever need, for example, to support ancient versions of VSCode.
   base64?: string;
-  byteLength?: number;
 };
