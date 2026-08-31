252f6bb311e0b81b8ac1f0e5a51540fe9bf0aaa9
Roman Dyro <tollemet@gmail.com>
2026-08-31 16:12:41 +0200

    Add basics for `ReactiveStore`

--- src/features/webview/store/ReactiveStore.ts
+++ src/features/webview/store/ReactiveStore.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { BufferItemData, BufferItemRegistry } from '../../buffer';
+
 export type ReactiveStoreEvent<Type extends string = string> = Event & {
   type: Type;
   target: ReactiveStore;
@@ -11,7 +14,21 @@ export type ReactiveStoreEvent<Type extends string = string> = Event & {
  * `listenTo(ReactiveStoreInstance, 'example-event', (event) => { ... })`
  */
 export class ReactiveStore<Type extends string = string> extends EventTarget {
+  constructor(
+    private readonly bufferItemRegistry: BufferItemRegistry,
+  ) {
+    super();
+  }
+
   public dispatchEvent(event: ReactiveStoreEvent<Type>): boolean {
     return super.dispatchEvent(event);
+  }
+
+  public addItems(items: BufferItemData[]): void {
+    this.bufferItemRegistry.upsert(items);
+
+    console.log('ReactiveStore: Added items to registry:', items);
+
+    this.dispatchEvent(new CustomEvent('items', { detail: items }) as any);
   }
 }
