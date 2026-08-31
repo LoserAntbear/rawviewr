664d3b9f5d504e76770b3f3d8266dc4afd8fc136
Roman Dyro <tollemet@gmail.com>
2026-08-31 16:14:19 +0200

    Add ref lookup and mounting to `RIVHTMLElement`

--- src/features/webview/ui/webcomponents/RIVHTMLElement.ts
+++ src/features/webview/ui/webcomponents/RIVHTMLElement.ts
@@ -1,10 +1,13 @@
 import type { WebviewCommand } from '../../commands/types';
 import { RIV_COMMAND_EVENT_ID } from '../../commands/definitions';
 import { WebviewDisposableStore } from '../../disposable/WebviewDisposableStore';
+import { nullishCoalesce } from '@utils/coalesce';
 
 export abstract class RIVHTMLElement extends HTMLElement {
   public static readonly tagName: string;
   protected readonly disposableStore = new WebviewDisposableStore();
+
+  protected readonly refs = new Map<string, HTMLElement>();
 
   public disconnectedCallback(): void {
     this.disposableStore.dispose();
@@ -18,5 +21,31 @@ export abstract class RIVHTMLElement extends HTMLElement {
         detail: command,
       }),
     );
+  }
+
+  protected mount(template: string): void {
+    const templateElement = document.createElement('template');
+
+    templateElement.innerHTML = template;
+
+    this.attachShadow({ mode: 'open' })
+      .appendChild(document.importNode(templateElement.content, true));
+  }
+
+  protected ref<T extends HTMLElement>(id: string): T | null {
+    return nullishCoalesce(
+      this.refs.get(id) as T ?? null,
+      this.queryAndCacheRef<T>(id),
+    ) as T | null;
+  }
+
+  protected queryAndCacheRef<T extends HTMLElement>(id: string): T | null {
+    const refElement = this.shadowRoot?.getElementById(id) as T | null;
+
+    if (refElement) {
+      this.refs.set(id, refElement);
+    }
+
+    return refElement;
   }
 }
