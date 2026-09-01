84a2d80e61043e78211dec70376ef5a981115692
Roman Dyro <tollemet@gmail.com>
2026-09-01 23:29:01 +0200

    Add new `RIVTags`

--- src/features/webview/ui/webcomponents/definitions.ts
+++ src/features/webview/ui/webcomponents/definitions.ts
@@ -2,5 +2,7 @@ export enum RIVTags {
   App = 'riv-app-component',
   Image = 'riv-image-component',
   Toolbar = 'riv-toolbar-component',
+  Gallery = 'riv-gallery-component',
   MainView = 'riv-main-view-component',
+  SingleView = 'riv-single-view-component',
 }
