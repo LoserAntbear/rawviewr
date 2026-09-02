61b63ad32d20b0caf571229172a9fb644de11510
Roman Dyro <tollemet@gmail.com>
2026-09-03 00:58:46 +0200

    Add `TypedEventTarget`

---
+++ src/features/webview/store/TypedEventTarget.ts
@@ -0,0 +1,34 @@
+type EventMap = Record<string, Event>;
+type Listener<T> = (event: CustomEvent<T>) => void;
+type UnsubscribeCallback = () => void;
+
+export class TypedEventTarget<
+  Map extends EventMap,
+  Key extends keyof Map & string = keyof Map & string,
+> extends EventTarget {
+  public on<K extends Key>(
+    type: K,
+    listener: Listener<Map[K]>,
+    options?: AddEventListenerOptions | boolean,
+  ): UnsubscribeCallback {
+    this.addEventListener(type, listener as EventListener, options);
+
+    return this.off.bind(this, type, listener as EventListener, options);
+  }
+
+  public off<K extends Key>(
+    type: K,
+    listener: Listener<Map[K]>,
+    options?: EventListenerOptions | boolean,
+  ): void {
+    this.removeEventListener(type, listener as EventListener, options);
+  }
+
+  public emit<K extends Key>(type: K, ...args: Map[K] extends void ? [] : [detail: Map[K]]): void {
+    const detail = args[0] as Map[K];
+    const event = new CustomEvent(type, { detail });
+
+    this.dispatchEvent(event);
+  }
+
+}
