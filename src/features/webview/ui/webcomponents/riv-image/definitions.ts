ce6d890314434e6fd1d72d52cea7368debd6f1d1
Roman Dyro <tollemet@gmail.com>
2026-09-08 17:11:03 +0200

    Add definitions for RIVImage state

---
+++ src/features/webview/ui/webcomponents/riv-image/definitions.ts
@@ -0,0 +1,11 @@
+export enum RIVImageStateKind {
+  Empty = 'empty',
+  Error = 'error',
+  Painted = 'painted',
+  Loading = 'loading',
+}
+
+export enum RIVImageStateTransition {
+  Failed = 'failed',
+  Resolved = 'resolved',
+}
