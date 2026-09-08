6f739f28fbfa8f3103389baf81ecdf12713d359b
Roman Dyro <tollemet@gmail.com>
2026-09-09 00:22:39 +0200

    Implement `ViewSlice`

---
+++ src/features/webview/store/slice/ViewSlice.ts
@@ -0,0 +1,33 @@
+import type { GalleryViewMode } from '../../ui/webcomponents/types';
+
+import { StoreSliceId } from '../definitions';
+import { StoreSlice } from './StoreSlice';
+import type { StoreSliceEventMap } from './types';
+
+export type ViewState = {
+  readonly mode: GalleryViewMode;
+  readonly selectedId: string | null;
+};
+export type ViewSliceEvents = StoreSliceEventMap<StoreSliceId.View, ViewState>;
+
+export class ViewSlice extends StoreSlice<StoreSliceId.View, ViewState> {
+  constructor() {
+    super(StoreSliceId.View, { mode: 'single', selectedId: null });
+  }
+
+  public get mode(): GalleryViewMode {
+    return this.get().mode;
+  }
+
+  public get selectedId(): string | null {
+    return this.get().selectedId;
+  }
+
+  public setMode(mode: GalleryViewMode): void {
+    this.patch({ mode });
+  }
+
+  public select(selectedId: string | null): void {
+    this.patch({ selectedId });
+  }
+}
