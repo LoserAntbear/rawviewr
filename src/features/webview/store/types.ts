d9567eca4d28f6f4f24883221f3a624472803d97
Roman Dyro <tollemet@gmail.com>
2026-09-08 23:59:20 +0200

    Add selectors typings

--- src/features/webview/store/types.ts
+++ src/features/webview/store/types.ts
@@ -5,7 +5,7 @@ import type { ViewSlice, ViewSliceEvents } from './slice/ViewSlice';
 import type { DecodeSlice, DecodeSliceEvents } from './slice/DecodeSlice';
 import type { ReactiveStore } from './ReactiveStore';
 import type { StoreSliceId } from './definitions';
-import type { StoreSelectors } from './selectors';
+import type { STORE_SELECTORS } from './selectors';
 
 export type SliceLike<TName extends string = string> = {
   readonly name: TName;
@@ -22,10 +22,21 @@ export type StoreSlices = {
 };
 export type StoreEventMap = ItemsSliceEvents & ViewSliceEvents & DecodeSliceEvents;
 
+export type Selector<TState, TResult, TArgs extends unknown[] = []> = (
+  state: TState,
+  ...args: TArgs
+) => TResult;
+export type SelectorMap<TState> = Readonly<Record<string, Selector<TState, unknown, never[]>>>;
+export type BoundSelectors<TState, TSelectors extends SelectorMap<TState>> = {
+  readonly [K in keyof TSelectors]: TSelectors[K] extends Selector<TState, infer TResult, infer TArgs>
+    ? (...args: TArgs) => TResult
+    : never;
+};
+
 export type AppState<TSlices extends SliceMap> = {
   readonly [K in keyof TSlices]: ReturnType<TSlices[K]['get']>;
 };
-export type AppStore = ReactiveStore<StoreSelectors, StoreSlices, StoreEventMap>;
+export type AppStore = ReactiveStore<typeof STORE_SELECTORS, StoreSlices, StoreEventMap>;
 export type AppStoreState = AppState<StoreSlices>;
 export type AppBus = TypedEventTarget<StoreEventMap>;
 
