acca58f08d8ac6e1b8983422cabaa5e1778efd29
Roman Dyro <tollemet@gmail.com>
2026-09-12 02:19:30 +0200

    Add `formatRegistry` to `WebviewContext`

--- src/features/webview/webviewContext/types.ts
+++ src/features/webview/webviewContext/types.ts
@@ -1,5 +1,7 @@
+import type { FormatRegistry } from '@features/image/format/FormatRegistry';
 import type { AppStore } from '../store/types';
 
 export interface WebviewContext {
   readonly store: AppStore;
+  readonly formatRegistry: FormatRegistry;
 }
