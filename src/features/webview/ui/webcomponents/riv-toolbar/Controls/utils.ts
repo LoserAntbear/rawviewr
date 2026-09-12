5f031f394dcf8ded1fb5fb4d47e3b8fb460b7469
Roman Dyro <tollemet@gmail.com>
2026-09-13 00:29:34 +0200

    Add `mapControlsToValues` control util

--- src/features/webview/ui/webcomponents/riv-toolbar/Controls/utils.ts
+++ src/features/webview/ui/webcomponents/riv-toolbar/Controls/utils.ts
@@ -1,5 +1,12 @@
-import { TOOLBAR_CONTROLS_BY_ID } from './controls';
+import { DecodeOptions } from '@features/image/imageDecoder/types';
+import { TOOLBAR_CONTROLS, TOOLBAR_CONTROLS_BY_ID } from './controls';
 
 export function getControlDefinitionForElement(element: HTMLElement) {
   return element.dataset.controlId ? TOOLBAR_CONTROLS_BY_ID.get(element.dataset.controlId) : undefined;
+}
+
+export function mapControlsToValues(options: DecodeOptions) {
+  return Object.fromEntries(
+    TOOLBAR_CONTROLS.map((control) => [control.id, control.toValueFromDecodeOptions(options)]),
+  );
 }
