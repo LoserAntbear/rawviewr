5d2c7caf956ed32d188d8202548219747cc43359
Roman Dyro <tollemet@gmail.com>
2026-09-15 20:21:24 +0200

    Update `WebviewHostMessage` & `WebviewMessage` types

--- src/features/webview/webviewHost/types.ts
+++ src/features/webview/webviewHost/types.ts
@@ -1,15 +1,18 @@
 import type { BufferItemData } from '../../buffer';
+import type { DecodeOptions } from '@features/image/imageDecoder/types';
 import type { GalleryViewMode } from '../ui/webcomponents/types';
+import type { ExportFormat } from '@definitions/exportFormats';
 
 export type WebviewHostMessageType = WebviewHostMessage['type'];
 export type WebviewHostMessage =
-  | { type: 'session'; viewMode: GalleryViewMode; }
   | { type: 'items'; items: BufferItemData[]; }
-  | { type: 'error'; message: string; };
+  | { type: 'export'; format: ExportFormat; }
+  | { type: 'status:error'; message: string; }
+  | { type: 'session:start'; viewMode: GalleryViewMode; decodeOptions: DecodeOptions; };
 
 export type WebviewMessageType = WebviewMessage['type'];
 export type WebviewMessage =
   | { type: 'app:ready' }
-  | { type: 'openItem'; id: string }
-  | { type: 'png'; name: string; base64: string }
-  | { type: 'status'; level: 'info' | 'warn' | 'error'; message: string };
+  | { type: 'gallery:openItem'; id: string }
+  | { type: 'export:png'; name: string; base64: string }
+  | { type: 'app:status'; level: 'info' | 'warn' | 'error'; message: string };
