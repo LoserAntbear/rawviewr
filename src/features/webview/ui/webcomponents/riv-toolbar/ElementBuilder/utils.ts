23a05023df324594796dc3bfa53995df8bbfd028
Roman Dyro <tollemet@gmail.com>
2026-09-17 01:31:47 +0200

    Fix `readElementValue` helper read

--- src/features/webview/ui/webcomponents/riv-toolbar/ElementBuilder/utils.ts
+++ src/features/webview/ui/webcomponents/riv-toolbar/ElementBuilder/utils.ts
@@ -1,17 +1,11 @@
-export function getElementValueIfAny(element: HTMLElement): string {
-  if (Object.hasOwn(element, 'value')) {
-    return (element as HTMLInputElement | HTMLSelectElement).value;
+export function readElementValue(element: HTMLElement): string {
+  if (element instanceof HTMLInputElement && element.type === 'checkbox') {
+    return element.checked ? 'true' : '';
+  }
+
+  if (element instanceof HTMLInputElement || element instanceof HTMLSelectElement) {
+    return element.value;
   }
 
   return '';
-}
-
-export function readElementValue(element: HTMLElement): string {
-  let result = getElementValueIfAny(element);
-
-  if (element instanceof HTMLInputElement &&element.type === 'checkbox') {
-    result = element.checked ? 'true' : '';
-  }
-
-  return result;
 }
