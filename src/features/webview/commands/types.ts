3db7a0814ee530509f7f43a02974b6006e9e35d5
Roman Dyro <tollemet@gmail.com>
2026-09-17 01:07:27 +0200

    Add `ExportRequest` WebviewCommand

--- src/features/webview/commands/types.ts
+++ src/features/webview/commands/types.ts
@@ -2,6 +2,7 @@ import type { WebviewCommandType } from './definitions';
 
 export type WebviewCommand =
   | { type: WebviewCommandType.AppReady; }
+  | { type: WebviewCommandType.ExportRequest; }
   | { type: WebviewCommandType.GalleryOpenItem; payload: string; }
   | { type: WebviewCommandType.WebviewConnected; payload: string; };
 
