be16b3f91e2c730389ccf85ff512ad3cc8fe48b6
Roman Dyro <tollemet@gmail.com>
2026-09-21 14:52:05 +0200

    Remove now obsolete `nullishCoalesce`

--- src/utils/coalesce.ts
+++
@@ -1,9 +0,0 @@
-export function nullishCoalesce<T>(...values: (T | null | undefined)[]): T | null {
-  for (const value of values) {
-    if (value !== null) {
-      return value || null;
-    }
-  }
-
-  return null;
-}
