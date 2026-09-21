cd26858353d2dd0286270401f4bef53657e10e87
Roman Dyro <tollemet@gmail.com>
2026-09-21 21:58:05 +0200

    Add `resolveGeometry` and geometry argument to `WebviewImageDecoder`

--- src/features/webview/image/WebviewImageDecoder.ts
+++ src/features/webview/image/WebviewImageDecoder.ts
@@ -1,5 +1,6 @@
 import { ImageDecoder } from '@features/image/imageDecoder/ImageDecoder';
 import type { DecodeOptions, DecodedImage } from '@features/image/imageDecoder/types';
+import type { Geometry } from '@features/image/imageDecoder/imagePreparation/types';
 import type { FormatRegistry } from '@features/image/format/FormatRegistry';
 import { withAbortSignalCheck } from '@utils/abort';
 
@@ -9,14 +10,19 @@ export class WebviewImageDecoder {
     private readonly decoder: ImageDecoder = new ImageDecoder(formatRegistry),
   ) {}
 
+  public resolveGeometry(data: ArrayBuffer, options: DecodeOptions): Geometry {
+    return this.decoder.resolveGeometry(new Uint8Array(data), options);
+  }
+
   public async decode(
     data: ArrayBuffer,
     options: DecodeOptions,
     signal?: AbortSignal,
+    geometry?: Geometry,
   ): Promise<ImageBitmap> {
     signal?.throwIfAborted();
 
-    const { image } = this.decoder.decode(new Uint8Array(data), options);
+    const { image } = this.decoder.decode(new Uint8Array(data), options, geometry);
 
     // Decoding can take long
     return withAbortSignalCheck(
