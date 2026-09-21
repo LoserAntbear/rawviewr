8d0c6b92ae7f4b0076889dabace08a6a5f72b391
Roman Dyro <tollemet@gmail.com>
2026-09-21 14:58:09 +0200

    Update `InfoMessageController` to support attemptance utils to solidify the flow and respect new handling strategy

--- src/features/infoMessage/InfoMessageController.ts
+++ src/features/infoMessage/InfoMessageController.ts
@@ -1,46 +1,45 @@
 import * as vscode from 'vscode';
 
+import { attemptDetached } from '@utils/attempt';
+
 type InfoMessage =
   | { level: 'warn' | 'error'; message: string; }
   | { level: 'info'; message: string; items?: string[] };
 export type InfoMessageLevel = InfoMessage['level'];
 
 export class InfoMessageController {
-  public static async handleMessage(message: InfoMessage): Promise<void> {
+  public static handleMessage(message: InfoMessage): void {
     switch (message.level) {
       case 'info':
-        await this.showInfo(message.message, message.items);
+        this.showInfo(message.message, message.items);
         break;
       case 'warn':
-        await this.showWarning(message.message);
+        this.showWarning(message.message);
         break;
       case 'error':
-        await this.showError(message.message);
+        this.showError(message.message);
         break;
     }
   }
 
-  public static async showInfo(message: string, items?: string[]): Promise<void> {
-    try {
-      return await vscode.window.showInformationMessage(message, ...(items ?? [])).then(() => {});
-    } catch (error) {
-      console.error(`Failed to show info message: ${error}`);
-    }
+  public static showInfo(message: string, items?: string[]): void {
+    attemptDetached(
+      () => vscode.window.showInformationMessage(message, ...(items ?? [])),
+      (error) => console.error(`Failed to show info message: ${error}`),
+    );
   }
 
-  public static async showWarning(message: string): Promise<void> {
-    try {
-      return await vscode.window.showWarningMessage(message).then(() => {});
-    } catch (error) {
-      console.error(`Failed to show warning message: ${error}`);
-    }
+  public static showWarning(message: string): void {
+    attemptDetached(
+      () => vscode.window.showWarningMessage(message),
+      (error) => console.error(`Failed to show warning message: ${error}`),
+    );
   }
 
-  public static async showError(message: string): Promise<void> {
-    try {
-      return await vscode.window.showErrorMessage(message).then(() => {});
-    } catch (error) {
-      console.error(`Failed to show error message: ${error}`);
-    }
+  public static showError(message: string): void {
+    attemptDetached(
+      () => vscode.window.showErrorMessage(message),
+      (error) => console.error(`Failed to show error message: ${error}`),
+    );
   }
 }
