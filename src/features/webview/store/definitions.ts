8db752f3c812adc592b329ab83811bfaa7b1d51c
Roman Dyro <tollemet@gmail.com>
2026-09-21 21:59:10 +0200

    Add `Images`-relate store const

--- src/features/webview/store/definitions.ts
+++ src/features/webview/store/definitions.ts
@@ -4,10 +4,12 @@ export enum StoreSliceId {
   View = 'view',
   Items = 'items',
   Decode = 'decode',
+  Images = 'images',
 }
 
 export const StoreEvent = {
   ViewChange: `${StoreSliceId.View}:${StoreSliceEvent.Change}`,
   ItemsChange: `${StoreSliceId.Items}:${StoreSliceEvent.Change}`,
   DecodeChange: `${StoreSliceId.Decode}:${StoreSliceEvent.Change}`,
+  ImagesChange: `${StoreSliceId.Images}:${StoreSliceEvent.Change}`,
 } as const;
