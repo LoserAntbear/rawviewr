a076cbcd94f2104f1e2d30efc752941d09d55dd5
Roman Dyro <tollemet@gmail.com>
2026-09-22 15:38:29 +0200

    Update imageslice decode api

--- src/features/webview/store/slice/ImagesSlice/ImagesSlice.ts
+++ src/features/webview/store/slice/ImagesSlice/ImagesSlice.ts
@@ -1,8 +1,13 @@
+import { FileSource } from '@features/webview/types';
 import { StoreSliceId } from '../../definitions';
 import { StoreSlice } from '../StoreSlice';
 import type { ImageItem, ImagesState } from './types';
 import { retireImageBitmap } from './utils';
 import { WebviewImageDecoder } from '@features/webview/image/WebviewImageDecoder';
+import { FileValidator } from '@features/file/FileValidator';
+import { BufferItem } from '@features/buffer/BufferItem';
+import { DecodeOptions } from '@features/image/imageDecoder/types';
+import { InfoMessageController } from '@features/infoMessage/InfoMessageController';
 
 /**
  * Keeps and handles the data about PROCESSED images.
@@ -60,10 +65,26 @@ export class ImagesSlice extends StoreSlice<StoreSliceId.Images, ImagesState> {
     this.patch({ selectedId });
   }
 
-  public async decodeFromSource(id: string, source: ArrayBuffer): Promise<void> {
-    const image = await this.decoder.decode(source);
+  public async decodeFromSource(
+    source: FileSource,
+    options: DecodeOptions,
+  ): Promise<void> {
+    try {
+      const bufferItem = await BufferItem.fromFileSource(source);
 
-    this.put(id, image);
+      if (!FileValidator.isValidFileSize(bufferItem.data.byteLength)) {
+        throw new Error(`File size exceeds the maximum allowed size of ${FileValidator.maxFileSizeMB} MB.`);
+      }
+
+      const image = await this.decoder.decode(bufferItem.data, options);
+
+      this.put(source.id, { kind: "ready", ...image });
+    } catch (error) {
+      InfoMessageController.showError(`Failed to decode image from source: ${error}`);
+      const message = Object.hasOwn((error as object), 'message') ? (error as Error).message : String(error);
+
+      this.put(source.id, { kind: "failed", message });
+    }
   }
 
   private putSingle(id: string, image: ImageItem, byId: Map<string, ImageItem>): void {
