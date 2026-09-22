4c292514d302b58f46a9bd5eacb8764f4428f681
Roman Dyro <tollemet@gmail.com>
2026-09-22 14:11:10 +0200

    Fix imports for reactions

--- src/features/webview/store/createWebviewStore.ts
+++ src/features/webview/store/createWebviewStore.ts
@@ -1,5 +1,4 @@
 import { WebviewDisposableStore } from '../disposable/WebviewDisposableStore';
-import { WebviewImageDecoder } from '../image/WebviewImageDecoder';
 import { TypedEventTarget } from './TypedEventTarget';
 
 import { ReactiveStore } from './ReactiveStore';
@@ -7,25 +6,23 @@ import { STORE_SELECTORS } from './selectors';
 import { StoreSliceId } from './definitions';
 import { SourcesSlice } from './slice/ItemsSlice';
 import { ViewSlice } from './slice/ViewSlice';
-import { DecodeSlice } from './slice/DecodeSlice';
 import { ImagesSlice } from './slice/ImagesSlice/ImagesSlice';
 import { STORE_REACTIONS } from './reactions';
 import type { FormatRegistry } from '@features/image/format/FormatRegistry';
 import type { AppStore, StoreSlices, StoreReaction } from './types';
 
 
-function createDefaultSlices(formatRegistry: FormatRegistry): StoreSlices {
+function createDefaultSlices(): StoreSlices {
   return {
     [StoreSliceId.View]: new ViewSlice(),
     [StoreSliceId.Images]: new ImagesSlice(),
     [StoreSliceId.Sources]: new SourcesSlice(),
-    [StoreSliceId.Decode]: new DecodeSlice(new WebviewImageDecoder(formatRegistry)),
   };
 }
 
 export function createWebviewStore(
-  formatRegistry: FormatRegistry,
-  slices: StoreSlices = createDefaultSlices(formatRegistry),
+  _formatRegistry: FormatRegistry,
+  slices: StoreSlices = createDefaultSlices(),
   reactions: readonly StoreReaction[] = STORE_REACTIONS,
 ): { store: AppStore; disposables: WebviewDisposableStore } {
   const disposables = new WebviewDisposableStore();
